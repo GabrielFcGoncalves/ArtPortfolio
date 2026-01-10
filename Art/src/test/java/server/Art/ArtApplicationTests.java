7dfa9f3619ecb945b46be0877be9859301f1af3a
GabrielFcGoncalves <gabfacgon3@gmail.com>
2026-01-10 10:25:19 +0000

    feature/ Project structure

---
+++ Art/src/test/java/server/Art/ArtApplicationTests.java
@@ -0,0 +1,13 @@
+package server.Art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
